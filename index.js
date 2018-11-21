1e9235ffbae3101cd5aa294df001c41fd6aede4b
Mikdanjey <mikdanjey@gmail.com>
2018-11-22 01:17:06 +0530

    ok

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-let jsonServer  = require('json-server');
+const path = require('path')
+const fs = require('fs')
+const _ = require('lodash')
+const jsonServer = require('json-server')
 
-let server      = jsonServer.create();
-let router      = jsonServer.router(require('./database_server.js')());
-let middlewares = jsonServer.defaults();
+const server = jsonServer.create()
+const port = 3002
 
+let obj = {}
+let files = fs.readdirSync(path.resolve(__dirname, './db_servers/'))
 
-const host_name = 'http://localhost';
-const host_port = 3001;
+files.forEach((file) => {
+  if (file.indexOf('.json') > -1) {
+    _.extend(obj, require(path.resolve(__dirname, './db_servers/', file)))
+  }
+})
 
-server.use(middlewares);
-server.use(router);
-server.listen(3001, function () {
-  console.log('Multi DB - JSON Server is running.');
-  console.log(host_name + ':' + host_port);
-});
+const router = jsonServer.router(obj)
+
+server.use(jsonServer.defaults())
+server.use(router)
+
+server.listen(port, () => {
+  console.log(`JSON Server is running at ${port}`);
+  console.log(`http://localhost:${port}`);
+})
